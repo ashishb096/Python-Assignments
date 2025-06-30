2c09b8a2f45ddaddcc0e1c261eee707446e69335
Ashish Bisht <73742377+ashishb096@users.noreply.github.com>
2025-06-30 11:13:59 +0530

    Add files via upload

---
+++ Assignments/Assiggnment 3 Functions & Modules in Pyhton/Task2.py
@@ -0,0 +1,8 @@
+import math
+
+n= int(input("Enter the number:"))
+
+sqrt=math.sqrt(n)
+loge=math.log(n,2.71828)
+sine=math.sin(n)
+print("square root: ",sqrt,"\n logarithm: ", loge, "\nSine ", sine)
